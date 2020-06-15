7119fa24206d1fefef1a42668c4cf18572ed6bc3
Maxim van Dijk <mvandijk303@gmail.com>
2020-06-15 20:10:11 +0200

    Make server serve index no matter what

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ var http = require('http')
 var url = require('url')
 var fs = require('fs')
 
-http.createServer(function (request, response) {
+http.createServer(serve).listen(80);
+
+
+function serve(request, response) {
     var requestUrl = url.parse(request.url)
+    if (requestUrl.pathname === '/')
+        requestUrl.pathname = 'index.html';
     response.writeHead(200)
 
     let dir = __dirname + '/build/';
@@ -12,14 +17,16 @@ http.createServer(function (request, response) {
     var stream = fs.createReadStream(dir + requestUrl.pathname);
 
     stream.on('error', function(err) {
-        console.log(err.message);
-        response.writeHead(404, {"Content-Type": "text/plain"});
-        response.write(err.message);
-        response.end();
+        console.log(err.message, request.url);
+        request.url = '/index.html';
+        return serve(request, response);
+        // response.writeHead(404, {"Content-Type": "text/plain"});
+        // response.write(err.message);
+        // response.end();
     });
 
     stream.on('open', function() {
         stream.pipe(response);
     });
 
-}).listen(8888);
+}
