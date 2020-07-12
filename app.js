b577c8f7fca539fdf9c1c051b56f3be4093e405c
Maxim van Dijk <mvandijk303@gmail.com>
2020-07-12 15:20:18 +0100

    test

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ let path = require('path');
 let app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/src/views'));
-app.use(express.static('public'));
+
+// app.use(express.static('public'));
+console.log(__dirname)
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/simply-sanskrit', express.static(path.join(__dirname, 'public')));
 
 
 app.get('/simply-sanskrit', (req, res) => {
--- edit.bat
+++ edit.bat
@@ -1,6 +1,6 @@
 @echo off
 start sh.exe --login -c "gulp; gulp watch; read -n 1 \"Press le any key.""
-start sh.exe --login -c "node server.js; read -n 1 \"Press le any key.""
-start "" "http://localhost/index.html"
+start sh.exe --login -c "node app.js; read -n 1 \"Press le any key.""
+start "" "http://localhost/"
 start explorer.exe "%CD%"
 atom ./
